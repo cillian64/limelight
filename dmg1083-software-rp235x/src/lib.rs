0a8af0543577da23a5f12e5afd32ab28dafc760a
David Turner <git@dwt27.co.uk>
2025-02-23 11:25:40 +0000

    Attempt at rp235x firmware

---
+++ dmg1083-software-rp235x/src/lib.rs
@@ -0,0 +1,210 @@
+#![no_std]
+
+use cortex_m::asm::nop;
+use dmg1083::{PanelData, ADDRESSES, IC_COUNT, OUTPUTS, SCAN_LINES};
+use embedded_hal::digital::v2::OutputPin;
+use mbi5153::PanelState;
+use misc_hacks::InvertedPin;
+use rp235x_hal::gpio::{DynPinId, DynPullType, FunctionSioOutput, Pin, PullType};
+use rp235x_hal::multicore::{Multicore, Stack};
+use rp235x_hal::pac::{PPB, PSM};
+use rp235x_hal::sio::SioFifo;
+use rp235x_hal::{pac, Sio};
+
+type Outpin = Pin<DynPinId, FunctionSioOutput, DynPullType>;
+type OutpinI = InvertedPin<Outpin>;
+
+const CMD_STOP_GCLK: u32 = 0x42;
+const CMD_RESTART_GCLK: u32 = 0x43;
+const RESP_ACK: u32 = 0x11;
+const RESP_ONLINE: u32 = 0x12;
+
+/// `Scanouter` has the address lines and grayscale clock line (GCLK).
+struct Scanouter {
+    /// Addresses A through E.
+    addresses: [OutpinI; 5],
+    gclk: OutpinI,
+    fifo: Option<SioFifo>,
+    gclk_multiplier: bool,
+}
+
+impl Scanouter {
+    fn handle_stop(&mut self) {
+        self.fifo.as_mut().unwrap().write_blocking(RESP_ACK);
+        self.gclk.set_low().unwrap();
+        for a in self.addresses.iter_mut() {
+            a.set_low().unwrap()
+        }
+        assert_eq!(
+            self.fifo.as_mut().unwrap().read_blocking(),
+            CMD_RESTART_GCLK
+        );
+        self.fifo.as_mut().unwrap().write_blocking(RESP_ACK);
+    }
+
+    fn run(&mut self) {
+        let gclks_per_line = if self.gclk_multiplier { 257 } else { 513 };
+        if let Some(fifo) = self.fifo.as_mut() {
+            fifo.write_blocking(RESP_ONLINE);
+        }
+        loop {
+            for line in 0..SCAN_LINES {
+                for i in 0..5 {
+                    self.addresses[i]
+                        .set_state(((line >> i) & 1 > 0).into())
+                        .unwrap();
+                }
+
+                for _ in 0..gclks_per_line {
+                    self.gclk.set_low().unwrap();
+                    nop();
+                    self.gclk.set_high().unwrap();
+                    nop();
+                }
+
+                self.gclk.set_low().unwrap();
+                for i in 0..5 {
+                    self.addresses[i].set_low().unwrap();
+                }
+
+                for _ in 0..10 {
+                    nop();
+                }
+            }
+
+            if let Some(fifo) = self.fifo.as_mut() {
+                if fifo.is_read_ready() {
+                    let data = fifo.read();
+                    match data {
+                        Some(CMD_STOP_GCLK) => self.handle_stop(),
+                        Some(_x) => panic!("unexpected cmd {_x} on core1"),
+                        None => {} // that's odd
+                    }
+                }
+            }
+        }
+    }
+}
+
+pub struct Dmg1083 {
+    dclk: OutpinI,
+    le: OutpinI,
+    /// SDI inputs for R/G/B, 1 through 4.
+    sdi_rgb: [OutpinI; OUTPUTS],
+    scanout: Option<Scanouter>,
+    fifo: SioFifo,
+}
+
+impl Dmg1083 {
+    #[allow(clippy::too_many_arguments)]
+    pub fn new<U: PullType>(
+        dclk: Pin<DynPinId, FunctionSioOutput, U>,
+        gclk: Pin<DynPinId, FunctionSioOutput, U>,
+        latch: Pin<DynPinId, FunctionSioOutput, U>,
+        outputs: [Pin<DynPinId, FunctionSioOutput, U>; OUTPUTS],
+        addresses: [Pin<DynPinId, FunctionSioOutput, U>; ADDRESSES],
+        fifo: SioFifo,
+        psm: &mut PSM,
+        ppb: &mut PPB,
+        gclk_multiplier: bool,
+    ) -> Self {
+        let mut ret = Self {
+            scanout: Some(Scanouter {
+                addresses: addresses.map(|x| InvertedPin(x.into_pull_type())),
+                gclk: InvertedPin(gclk.into_pull_type()),
+                fifo: None,
+                gclk_multiplier,
+            }),
+            dclk: InvertedPin(dclk.into_pull_type()),
+            le: InvertedPin(latch.into_pull_type()),
+            sdi_rgb: outputs.map(|x| InvertedPin(x.into_pull_type())),
+            fifo,
+        };
+        ret.spawn_second_core(psm, ppb);
+        ret
+    }
+
+    fn stop_gclk(&mut self) {
+        self.fifo.write_blocking(CMD_STOP_GCLK);
+        assert_eq!(self.fifo.read_blocking(), RESP_ACK);
+    }
+
+    fn restart_gclk(&mut self) {
+        self.fifo.write_blocking(CMD_RESTART_GCLK);
+        assert_eq!(self.fifo.read_blocking(), RESP_ACK);
+    }
+
+    pub fn transmit_frame(&mut self, data: &PanelData) {
+        let state = PanelState::new(
+            [
+                &data.0[0].red,
+                &data.0[0].green,
+                &data.0[0].blue,
+                &data.0[1].red,
+                &data.0[1].green,
+                &data.0[1].blue,
+                &data.0[2].red,
+                &data.0[2].green,
+                &data.0[2].blue,
+                &data.0[3].red,
+                &data.0[3].green,
+                &data.0[3].blue,
+            ],
+            IC_COUNT as _,
+            0b1111_1111_1111_1100,
+        );
+        for outputs in state {
+            for (i, sdi) in outputs.sdis.into_iter().enumerate() {
+                self.sdi_rgb[i].set_state(sdi.into()).unwrap();
+            }
+            self.le.set_state(outputs.le.into()).unwrap();
+            self.dclk.set_state(outputs.dclk.into()).unwrap();
+        }
+
+        self.le.set_low().unwrap();
+        self.dclk.set_low().unwrap();
+    }
+
+    pub fn vsync(&mut self) {
+        self.stop_gclk();
+
+        self.le.set_high().unwrap();
+        nop();
+        self.dclk.set_high().unwrap();
+        nop();
+        self.dclk.set_low().unwrap();
+        nop();
+        self.dclk.set_high().unwrap();
+        nop();
+        self.dclk.set_low().unwrap();
+        nop();
+        self.le.set_low().unwrap();
+        nop();
+
+        self.restart_gclk();
+    }
+
+    fn spawn_second_core(&mut self, psm: &mut PSM, ppb: &mut PPB) {
+        let scanouter = self.scanout.take().unwrap();
+        let mut mc = Multicore::new(psm, ppb, &mut self.fifo);
+        mc.cores()[1]
+            .spawn(unsafe { &mut CORE1_STACK.mem }, move || {
+                core1_main(scanouter)
+            })
+            .unwrap();
+
+        assert_eq!(self.fifo.read_blocking(), RESP_ONLINE);
+    }
+}
+
+static mut CORE1_STACK: Stack<4096> = Stack::new();
+
+fn core1_main(mut scanouter: Scanouter) {
+    let pac = unsafe { pac::Peripherals::steal() };
+    // let _core = unsafe { pac::CorePeripherals::steal() };
+    let sio = Sio::new(pac.SIO);
+    // FIXME(eta): why can't I initialise the pins here??
+    scanouter.fifo = Some(sio.fifo);
+    scanouter.run();
+    unreachable!();
+}
--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -9,6 +9,23 @@ use rp235x_hal as hal;
 use embedded_hal::delay::DelayNs;
 use embedded_hal::digital::OutputPin;
 
+use dmg1083::{PanelData, SCAN_LINES};
+use dmg1083_software_rp235x::Dmg1083;
+use mbi5153::{Mbi5153Config, Mbi5153Config2};
+use misc_hacks::{InvertedPin, PinGroup, PinRef};
+
+use embedded_graphics::{
+    mono_font::{ascii::FONT_6X10, MonoTextStyle},
+    pixelcolor::BinaryColor,
+    pixelcolor::Rgb888,
+    prelude::*,
+    primitives::{
+        Circle, PrimitiveStyle, PrimitiveStyleBuilder, Rectangle, StrokeAlignment, Triangle,
+    },
+    text::{Alignment, Text},
+    mock_display::MockDisplay,
+};
+
 #[link_section = ".start_block"]
 #[used]
 pub static IMAGE_DEF: hal::block::ImageDef = hal::block::ImageDef::secure_exe();
@@ -43,12 +60,149 @@ fn main() -> ! {
 
     let mut led1_pin = pins.gpio26.into_push_pull_output();
     let mut led2_pin = pins.gpio0.into_push_pull_output();
+    led1_pin.set_high().unwrap();
+
+//    loop {
+//        led1_pin.set_high().unwrap();
+//        led2_pin.set_low().unwrap();
+//        timer.delay_ms(500);
+//        led1_pin.set_low().unwrap();
+//        led2_pin.set_high().unwrap();
+//        timer.delay_ms(500);
+//    }
+
+    // Address lines
+    let addr_a = pins.gpio2.into_push_pull_output().into_dyn_pin();
+    let addr_b = pins.gpio3.into_push_pull_output().into_dyn_pin();
+    let addr_c = pins.gpio4.into_push_pull_output().into_dyn_pin();
+    let addr_d = pins.gpio5.into_push_pull_output().into_dyn_pin();
+    let addr_e = pins.gpio6.into_push_pull_output().into_dyn_pin();
+
+    // Clocks
+    let gclk = pins.gpio7.into_push_pull_output().into_dyn_pin();
+    let mut dclk = pins.gpio8.into_push_pull_output().into_dyn_pin();
+    let mut latch = pins.gpio9.into_push_pull_output().into_dyn_pin();
+
+    // Data lines
+    let mut r1_sdi = pins.gpio10.into_push_pull_output().into_dyn_pin();
+    let mut g1_sdi = pins.gpio11.into_push_pull_output().into_dyn_pin();
+    let mut b1_sdi = pins.gpio12.into_push_pull_output().into_dyn_pin();
+    let mut r2_sdi = pins.gpio13.into_push_pull_output().into_dyn_pin();
+    let mut g2_sdi = pins.gpio14.into_push_pull_output().into_dyn_pin();
+    let mut b2_sdi = pins.gpio15.into_push_pull_output().into_dyn_pin();
+    // note schematic fuckery:
+    // G3 actually controls R3
+    // (R3 actually controls G3)
+    // B4 actually controls B3
+    // (B3 actually controls B4)
+    // G4 actually controls R4
+    // (R4 actually controls G4)
+    let mut r3_sdi = pins.gpio17.into_push_pull_output().into_dyn_pin();
+    let mut g3_sdi = pins.gpio16.into_push_pull_output().into_dyn_pin();
+    let mut b3_sdi = pins.gpio21.into_push_pull_output().into_dyn_pin();
+    let mut r4_sdi = pins.gpio20.into_push_pull_output().into_dyn_pin();
+    let mut g4_sdi = pins.gpio19.into_push_pull_output().into_dyn_pin();
+    let mut b4_sdi = pins.gpio18.into_push_pull_output().into_dyn_pin();
+    // (end schematic fuckery)
+
+    // SR pin that needs to be low to work
+    let mut sr = pins.gpio22.into_push_pull_output().into_dyn_pin();
+    sr.set_high().unwrap();
+
+    let sdis = PinGroup([
+        InvertedPin(PinRef(&mut r1_sdi)),
+        InvertedPin(PinRef(&mut r2_sdi)),
+        InvertedPin(PinRef(&mut r3_sdi)),
+        InvertedPin(PinRef(&mut r4_sdi)),
+        InvertedPin(PinRef(&mut g1_sdi)),
+        InvertedPin(PinRef(&mut g2_sdi)),
+        InvertedPin(PinRef(&mut g3_sdi)),
+        InvertedPin(PinRef(&mut g4_sdi)),
+        InvertedPin(PinRef(&mut b1_sdi)),
+        InvertedPin(PinRef(&mut b2_sdi)),
+        InvertedPin(PinRef(&mut b3_sdi)),
+        InvertedPin(PinRef(&mut b4_sdi)),
+    ]);
+
+    let config = Mbi5153Config {
+        scan_line_count: SCAN_LINES as _,
+        current_gain: 255,
+        gclk_multiplier: true,
+        lower_ghost_elimination: true,
+        ..Default::default()
+    };
+
+    dmg1083::configure_panel(
+        config,
+        InvertedPin(PinRef(&mut dclk)),
+        InvertedPin(PinRef(&mut latch)),
+        sdis,
+    )
+    .unwrap();
+
+    dmg1083::configure_panel_2(
+        InvertedPin(PinRef(&mut dclk)),
+        InvertedPin(PinRef(&mut latch)),
+        Mbi5153Config2::appnote_secret_sauce_r(),
+        PinGroup([
+            InvertedPin(PinRef(&mut r1_sdi)),
+            InvertedPin(PinRef(&mut r2_sdi)),
+            InvertedPin(PinRef(&mut r3_sdi)),
+            InvertedPin(PinRef(&mut r4_sdi)),
+        ]),
+        Mbi5153Config2::appnote_secret_sauce_gb(),
+        PinGroup([
+            InvertedPin(PinRef(&mut g1_sdi)),
+            InvertedPin(PinRef(&mut g2_sdi)),
+            InvertedPin(PinRef(&mut g3_sdi)),
+            InvertedPin(PinRef(&mut g4_sdi)),
+        ]),
+        Mbi5153Config2::appnote_secret_sauce_gb(),
+        PinGroup([
+            InvertedPin(PinRef(&mut b1_sdi)),
+            InvertedPin(PinRef(&mut b2_sdi)),
+            InvertedPin(PinRef(&mut b3_sdi)),
+            InvertedPin(PinRef(&mut b4_sdi)),
+        ]),
+    )
+    .unwrap();
+    // rprintln!("[+] initialising software driver");
+
+    let mut app = Dmg1083::new(
+        dclk,
+        gclk,
+        latch,
+        [
+            r1_sdi, g1_sdi, b1_sdi, r2_sdi, g2_sdi, b2_sdi, r3_sdi, g3_sdi, b3_sdi, r4_sdi, g4_sdi,
+            b4_sdi,
+        ],
+        [addr_a, addr_b, addr_c, addr_d, addr_e],
+        sio.fifo,
+        &mut pac.PSM,
+        &mut pac.PPB,
+        config.gclk_multiplier,
+    );
+
+    // SENDING DATA
+    let mut panel: PanelData;
+
+    let fill = PrimitiveStyle::with_fill(Rgb888::BLUE);
+
     loop {
-        led1_pin.set_high().unwrap();
-        led2_pin.set_low().unwrap();
-        timer.delay_ms(500);
-        led1_pin.set_low().unwrap();
-        led2_pin.set_high().unwrap();
-        timer.delay_ms(500);
+        panel = PanelData::default();
+
+        Rectangle::new(Point::new(10, 10), Size::new(6, 6))
+            .into_styled(fill)
+            .draw(&mut panel)
+            .unwrap();
+
+        Circle::new(Point::new(40, 40), 5)
+            .into_styled(fill)
+            .draw(&mut panel)
+            .unwrap();
+
+        app.transmit_frame(&panel);
+        app.vsync();
     }
+
 }
